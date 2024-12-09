f5d7c5942b6b86c4fbb35f17a153e0351146f36e
blanka111 <78907004+blanka111@users.noreply.github.com>
2024-12-09 18:15:37 +0100

    Add files via upload

---
+++ cypress/e2e/bdd_test.cy.js
@@ -0,0 +1,22 @@
+import {createStep, createSession, createIntercept, urlLoad, generateRandomText, sortResult} from './helper';
+import Lib from '../e2e/bdd_libary/libary';
+import Result from '../e2e/bdd_pages/searchResult';
+
+describe('bdd_UlovDomov', () => {
+    beforeEach(() => {
+        urlLoad('https://ud-fe.k8stage.ulovdomov.cz/');
+    });
+
+    it('1.Hledej nemovitost a vysortuj dle nahodneho kriteria', () => {
+        Lib.searchInCity();
+        Lib.sortResult();
+        createStep('rozkli prvni nemovitosti');
+        Result.tableFoundNemovitost().click();
+    });
+
+    it('2.Hledej nemovitost dle mesta a pak uprav filter Prodej /Byt ', () => {
+        Lib.searchInCity();
+        Lib.filterOptions();
+    });
+
+});
